a0a78344bc6e895edb91d72293947d3da21ee3b2
Pete Stenger <pete@stenger.io>
2023-01-06 23:33:28 -0500

    Start footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,62 @@
 import React from "react"
+import { DiscordSvg, GithubSvg, PwnySvg, YouTubeSvg, TwitterSvg } from "./Icons"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
-const Footer = () => (
+// SiteSiteMetadataSocialLinks
+
+const Footer = () => {
+  const icons : Map<string, JSX.Element> = new Map([
+    ["Twitter", <TwitterSvg />],
+    ["Youtube", <YouTubeSvg />],
+    ["GitHub", <GithubSvg />],
+    ["Discord", <DiscordSvg />]
+  ])
+
+  // Stopping here as I have some concerns with this design pattern :/
+  const data : Queries.SiteSiteMetadataSocialLinks[] = useStaticQuery(graphql`
+    query Nav {
+      site {
+        siteMetadata {
+          navLinks {
+            name
+            link
+          }
+          socialLinks {
+            name
+            link
+          }
+        }
+      }
+    }
+  `)
+
+  return (
   <footer>
-    <div className="container">
-      <p>
-        Copyright (c) SIGPwny. ACM@UIUC is a 501(c)(3) non-profit organization.
-      </p>
-      <p>Socials</p>
+    <div className="container flex flex-col-reverse md:flex-row">
+      <div className="flex flex-col">
+        <div>
+          <PwnySvg height="10em" /> 
+        </div>
+        <div>
+          <p className="text-xs">
+            Copyright (c) SIGPwny. ACM@UIUC is a 501(c)(3) non-profit organization.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row">
+        <div>
+          <h1>Sitemap</h1>
+          <Link to={""} className="truncate" activeClassName="font-bold">
+                
+          </Link>
+        </div>
+        <div>
+          <h1>Connect</h1>
+        </div>
+      </div>
     </div>
   </footer>
-)
+  )
+}
 
 export default Footer
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -71,7 +71,7 @@ const Nav = () => {
                 <div className="flex-1 flex items-center justify-center md:items-stretch md:justify-start">
                   <div className="flex-shrink-0 flex items-center">
                     <Link to="/">
-                      <PwnySvg />
+                      <PwnySvg height="48px" />
                     </Link>
                   </div>
                   <div className="hidden md:block md:ml-6 my-auto">
