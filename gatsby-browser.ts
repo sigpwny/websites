5de101a2227868c51dddfb6ef16c9ab3918671e2
Minh Duong <git@whitehoodhacker.net>
2023-04-19 18:45:24 -0500

    Add md.css

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,4 +1,5 @@
 import "./src/styles/main.css"
+import "./src/styles/md.css"
 
 import customWrapPageElement from "./src/components/WrapPageElements"
 export const wrapPageElement = customWrapPageElement
