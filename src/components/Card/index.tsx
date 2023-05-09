a445692b76535d9f32bec00b1cf68d15995b6b36
Minh Duong <git@whitehoodhacker.net>
2023-05-09 18:04:54 -0500

    Add overlay image to Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,15 +4,36 @@ import { GatsbyImage, StaticImage, getImage, IGatsbyImageData } from "gatsby-plu
 import "./card.css"
 import SmartLink from "../SmartLink"
 
-const Card = ({ heading, title, image, link }: CardProps) => {
+const Card = ({ heading, title, image, overlay_image, link }: CardProps) => {
   var card_component = (
     <div className="card h-100 grow use-color-text">
-      <div className="aspect-video flex align-middle">
-        {image && image.path && getImage(image.path) ? (
-          <GatsbyImage
-            image={getImage(image.path) as IGatsbyImageData}
-            alt={image.alt}
-          />
+      <div className="aspect-video grid">
+        {(image || overlay_image) ? (
+          <>
+            {image && image.path && getImage(image.path) && (
+              <GatsbyImage
+                image={getImage(image.path) as IGatsbyImageData}
+                alt={image.alt}
+                className="row-span-full col-span-full"
+              />
+            )}
+            {overlay_image && overlay_image.path && getImage(overlay_image.path) && (
+              <div className="row-span-full col-span-full overflow-hidden p-4 grid items-center">
+                {/* I spent a while to figure out that the combination of:
+                    - parent div: overflow-hidden
+                    - child div: max-h-full
+                    - child img: scale-down
+                    will resize the image in a nice way... I have no idea
+                    how it works.*/}
+                  <GatsbyImage
+                    image={getImage(overlay_image.path) as IGatsbyImageData}
+                    alt={overlay_image.alt}
+                    className="max-h-full"
+                    objectFit="scale-down"
+                  />
+              </div>
+            )}
+          </>
         ) : (
           <StaticImage
             src="./placeholder.png"
@@ -20,14 +41,20 @@ const Card = ({ heading, title, image, link }: CardProps) => {
           />
         )}
       </div>
-      <div className="p-2">
-        <p className="line-clamp-1 leading-4 m-0 font-mono font-size-small">
-          {heading}
-        </p>
-        <p className="line-clamp-2 leading-5 mb-1">
-          {title}
-        </p>
-      </div>
+      {(title || heading) && (
+        <div className="p-2">
+          {heading && (
+            <p className="line-clamp-1 leading-4 m-0 font-mono font-size-small">
+              {heading}
+            </p>
+          )}
+          {title && (
+            <p className="line-clamp-2 leading-5 mb-1">
+              {title}
+            </p>
+          )}
+        </div>
+      )}
     </div>
   )
   if (link) {
--- src/global-types.d.ts
+++ src/global-types.d.ts
@@ -4,8 +4,13 @@ interface Image {
 }
 
 interface CardProps {
-  heading: string
-  title: string
-  image: Image
+  heading?: string
+  title?: string
   link?: string
+  image?: Image
+  overlay_image?: Image
+  // images?: {
+  //   full?: Image
+  //   overlay?: Image
+  // }
 }
