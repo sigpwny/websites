ff42e4f019930c23db6f5691f37cd54263d4d8f1
Peter Stenger <peter.a.stenger@gmail.com>
2023-05-19 19:37:07 -0400

    add images, install remark images plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,4 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -7,21 +7,21 @@ const config: GatsbyConfig = {
     description: `SIGPwny is a student-run organization at the University of Illinois Urbana-Champaign focused on information security and privacy.`,
     image: `https://beta.sigpwny.com/pwny8-dark-512x512.png`,
     navLinks: [
-      { name: "SIGPwny", link: "/" },
-      { name: "About", link: "/about/" },
-      { name: "Meetings", link: "/meetings/" },
-      { name: "Events", link: "/events/" },
-      { name: "Sponsors", link: "/sponsors/" },
+      { name: 'SIGPwny', link: '/' },
+      { name: 'About', link: '/about/' },
+      { name: 'Meetings', link: '/meetings/' },
+      { name: 'Events', link: '/events/' },
+      { name: 'Sponsors', link: '/sponsors/' },
     ],
-    navCallToActionLinks: [{ name: "Join", link: "/join/" }],
+    navCallToActionLinks: [{ name: 'Join', link: '/join/' }],
     socialLinks: [
-      { name: "Discord", link: "https://discord.gg/cWcZ6a9" },
-      { name: "GitHub", link: "https://github.com/sigpwny" },
-      { name: "Twitter", link: "https://twitter.com/sigpwny" },
-      { name: "YouTube", link: "https://www.youtube.com/@sigpwny" },
+      { name: 'Discord', link: 'https://discord.gg/cWcZ6a9' },
+      { name: 'GitHub', link: 'https://github.com/sigpwny' },
+      { name: 'Twitter', link: 'https://twitter.com/sigpwny' },
+      { name: 'YouTube', link: 'https://www.youtube.com/@sigpwny' },
     ],
-    twitterUsername: "@sigpwny",
-    timezone: "America/Chicago",
+    twitterUsername: '@sigpwny',
+    timezone: 'America/Chicago',
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -29,93 +29,93 @@ const config: GatsbyConfig = {
   graphqlTypegen: true,
   plugins: [
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages_md",
-        path: "./src/pages_md/",
+        name: 'pages_md',
+        path: './src/pages_md/',
         ignore: [`**/.*`],
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "meetings",
-        path: "./content/meetings/",
+        name: 'meetings',
+        path: './content/meetings/',
         ignore: [`**/.*`],
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "events",
-        path: "./content/events/",
+        name: 'events',
+        path: './content/events/',
         ignore: [`**/.*`],
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "publications",
-        path: "./content/publications/",
+        name: 'publications',
+        path: './content/publications/',
         ignore: [`**/.*`],
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "admins",
-        path: "./content/profiles/admins/",
+        name: 'admins',
+        path: './content/profiles/admins/',
         ignore: [`**/.*`],
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "alumni",
-        path: "./content/profiles/alumni/",
+        name: 'alumni',
+        path: './content/profiles/alumni/',
         ignore: [`**/.*`],
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "helpers",
-        path: "./content/profiles/helpers/",
+        name: 'helpers',
+        path: './content/profiles/helpers/',
         ignore: [`**/.*`],
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "redirects",
-        path: "./content/redirects/",
+        name: 'redirects',
+        path: './content/redirects/',
         ignore: [`**/.*`],
       },
     },
-    "gatsby-plugin-image",
-    "gatsby-plugin-netlify",
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-sitemap",
-    "gatsby-transformer-json",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-image',
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-sitemap',
+    'gatsby-transformer-json',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-plugin-mdx", // https://github.com/gatsbyjs/gatsby/issues/37292
+      resolve: 'gatsby-plugin-mdx', // https://github.com/gatsbyjs/gatsby/issues/37292
       options: {
-        extensions: [".md", ".mdx"],
+        extensions: ['.md', '.mdx'],
         gatsbyRemarkPlugins: [
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              classPrefix: "language-",
-              inlineCodeMarker: ">",
+              classPrefix: 'language-',
+              inlineCodeMarker: '>',
               aliases: {},
               showLineNumbers: false,
             },
@@ -124,40 +124,40 @@ const config: GatsbyConfig = {
       },
     },
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "./src/images/logo/pwny8-48x48.png",
+        icon: './src/images/logo/pwny8-48x48.png',
         icons: [
           {
-            src: "./src/images/logo/pwny8-32x32.png",
-            sizes: "32x32",
-            type: "image/png",
+            src: './src/images/logo/pwny8-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
           },
           {
-            src: "./src/images/logo/pwny8-dark-128x128.png",
-            sizes: "128x128",
-            type: "image/png",
+            src: './src/images/logo/pwny8-dark-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
           },
           {
-            src: "./src/images/logo/pwny8-dark-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: './src/images/logo/pwny8-dark-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "./src/images/logo/pwny8-dark-256x256.png",
-            sizes: "256x256",
-            type: "image/png",
+            src: './src/images/logo/pwny8-dark-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
           },
           {
-            src: "./src/images/logo/pwny8-dark-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: './src/images/logo/pwny8-dark-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
         ],
       },
     },
     {
-      resolve: "gatsby-plugin-sharp",
+      resolve: 'gatsby-plugin-sharp',
       options: {
         defaults: {
           quality: 100,
