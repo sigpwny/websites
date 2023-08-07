659e99d5a5ec977c92c90f1980fa5c5e32bf1982
Minh Duong <git@whitehoodhacker.net>
2023-08-07 16:33:55 -0400

    Edit footer message

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -80,9 +80,7 @@ const Footer = () => {
         </div>
         <p>
           &copy; {new Date().getFullYear()} SIGPwny. ACM@UIUC is a 501(c)(3) non-profit organization.
-          This site is open source on <a href="https://github.com/sigpwny/sigpwny.com" target="_blank" rel="noopener noreferrer">
-            GitHub
-          </a>.
+          Site source on <a href="https://github.com/sigpwny/sigpwny.com" target="_blank" rel="noopener noreferrer">GitHub</a>.
         </p>
       </div>
     </footer>
