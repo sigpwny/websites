765554d85d260be37d3bd040c25a68328974a027
Minh Duong <git@whitehoodhacker.net>
2023-08-15 15:05:43 -0500

    wip: animated sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "gatsby";
 import { motion, LayoutGroup } from "framer-motion";
 import { ChevronRightRegular } from "@fluentui/react-icons";
-import _uniqueId from "lodash/uniqueId";
 
 import "./styles.css";
 
+// TODO: Add option to expand all items by default
 export const Sidebar = ({ root_items }: SidebarProps) => {
   return (
     <LayoutGroup>
@@ -22,43 +22,45 @@ export const SidebarItem = ({ item, depth }: {item: SidebarItem, depth?: number}
   const curr_depth = depth || 0;
   const padding = {paddingLeft: `${curr_depth}em`};
   const transition = {duration: 0.2, ease: "easeOut"};
-  if (item.items) {
-    const [id] = useState(_uniqueId("sidebar-dropdown-"));
+  if (item.items && item.items.length > 0) {
     const [expanded, setExpanded] = useState(false);
+    const subitems_ref = useRef<HTMLSpanElement>(null);
     const new_depth = curr_depth + 1;
     return (
-      <motion.label
+      <motion.span
         className="flex flex-col gap-1"
         transition={transition}
         layout="position"
       >
-        <input
-          type="checkbox"
-          className="peer opacity-0 absolute"
-          onChange={(e) => setExpanded(e.target.checked)}
-        />
-        <span
+        <button
           style={padding}
-          className="sidebar-item cursor-pointer peer-focus:" /* TODO: peer-checked: */
-          // onClick={() => setExpanded(!expanded)}
+          className={`sidebar-item cursor-pointer ${
+            !expanded && subitems_ref?.current?.querySelector(".active") ? "active" : ""
+          }`}
+          onClick={() => setExpanded(!expanded)}
         >
           <span className="indicator" />
           <motion.span
-            className="flex flex-shrink-0 flex-grow-0"
+            className="flex flex-shrink-0 flex-grow-0 rotate-90"
+            initial={false}
             animate={{rotate: expanded ? 90 : 0}}
             transition={transition}
           >
             <ChevronRightRegular className="flex flex-shrink-0 flex-grow-0 checked:hidden" />
           </motion.span>
           <span className="ml-2">{item.name}</span>
-        </span>
+        </button>
         <motion.span
-          className="flex flex-col gap-1 overflow-hidden h-0 peer-checked:h-auto"
+          ref={subitems_ref}
+          className="flex flex-col gap-1"
+          initial={false}
           animate={{
             height: expanded ? "auto" : 0,
             display: "flex",
+            overflowY: "hidden",
             transitionEnd: {
               display: expanded ? "flex" : "none",
+              overflowY: expanded ? "visible" : "hidden",
             }
           }}
           transition={transition}
@@ -68,7 +70,7 @@ export const SidebarItem = ({ item, depth }: {item: SidebarItem, depth?: number}
             <SidebarItem key={idx} item={subitem} depth={new_depth} />
           ))}
         </motion.span>
-      </motion.label>
+      </motion.span>
     );
   } else if (item.link) {
     return (
@@ -80,7 +82,7 @@ export const SidebarItem = ({ item, depth }: {item: SidebarItem, depth?: number}
           to={item.link}
           style={padding}
           className="sidebar-item"
-          activeClassName="bg-surface-100 active"
+          activeClassName="active"
         >
           <span className="indicator" />
           <span className="ml-2">{item.name}</span>
