265c7cd197318befa405b91107b29291238d002a
Pete Stenger <pete@stenger.io>
2023-10-09 19:46:50 -0500

    wip

--- netlify/edge-functions/hack.js
+++ netlify/edge-functions/hack.js
@@ -1,12 +1,6 @@
 export default async (req, context) => {
-    const url = new URL(req.url);
-    // #access_token
-    console.log(url, url.hash, url.hash.startsWith("#access_token"))
-    if (url.hash.startsWith("#access_token")) {
-        const access_token = new URL(req.url.replace("#", "?", 1)).searchParams.get("access_token")
-        // We have enough info, go back to the original URL
-        // Go to sigpwny.com/admin and set the cookie nf_jwt to the access_token
-        context.cookies.set({name: "nf_jwt", value: access_token, "sameSite": "None"})
+    if (context.cookies.get("nf_jwt")) {
+        context.cookies.set({name: "nf_jwt2", value: access_token, "sameSite": "None"})
         return Response.redirect("https://sigpwny.com/admin");
     }
     return;
