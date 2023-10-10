10bb768249e2dea2391fecf64b9113fd448e497b
Pete Stenger <pete@stenger.io>
2023-10-09 19:50:58 -0500

    wip

--- netlify/edge-functions/hack.js
+++ netlify/edge-functions/hack.js
@@ -1,11 +1,11 @@
 export default async (req, context) => {
     if (context.cookies.get("nf_jwt")) {
-        context.cookies.set({name: "nf_jwt2", value: access_token, "sameSite": "None"})
+        context.cookies.set({name: "nf_jwt2", value: context.cookies.get("nf_jwt"), "sameSite": "None"})
         return Response.redirect("https://sigpwny.com/admin");
     }
     return;
 };
 
 export const config = {
-  path: "/"
+  path: "/settings"
 };
