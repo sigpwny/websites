b682b23b086f6066cb766c693dc6b116292246ea
Pete Stenger <pete@stenger.io>
2023-10-09 20:05:50 -0500

    Revert "wip"
    This reverts commit df8bf5ba769b47e7989e66bcb99f8be38945b214.
    
    Revert "wip"
    
    This reverts commit 10bb768249e2dea2391fecf64b9113fd448e497b.
    
    Revert "wip"
    
    This reverts commit 265c7cd197318befa405b91107b29291238d002a.
    
    Revert "wip"
    
    This reverts commit c69fb92c8a69307e058d6b948ab4d13359135193.
    
    Revert "more hacking"
    
    This reverts commit 0ab4199e08bc750659aa3f22e11bf54a7950e047.

--- netlify/edge-functions/hack.js
+++ netlify/edge-functions/hack.js
@@ -1,12 +1,14 @@
 export default async (req, context) => {
-    
-    if (context.cookies.get("nf_jwt")) {
-        context.cookies.set({name: "nf_jwt2", value: context.cookies.get("nf_jwt"), sameSite: "None", secure: true})
+    const url = new URL(req.url);
+    if (url.searchParams.get("access_token") !== null) {
+        // We have enough info, go back to the original URL
+        // Go to sigpwny.com/admin and set the cookie nf_jwt to the access_token
+        context.cookies.set({"name": "nf_jwt", "value": url.searchParams.get("access_token"), "sameSite": "none"})
         return Response.redirect("https://sigpwny.com/admin");
     }
     return;
 };
 
 export const config = {
-  path: "/settings"
+  path: "/"
 };
