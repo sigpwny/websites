a8c96b22c1788f283d56767d3f9180b14b361545
Minh Duong <git@whitehoodhacker.net>
2024-08-20 12:52:25 -0500

    Fix vite copy location

--- astro.config.ts
+++ astro.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
         targets: [
           {
             src: 'src/content/meetings/*',
-            dest: 'meeting'
+            dest: 'meetings'
           }
         ]
       })
