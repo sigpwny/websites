d1a07d350d3fb5424b9d35eeab1c540db24eddaa
Minh Duong <git@whitehoodhacker.net>
2024-08-25 01:45:08 -0500

    Fix Event schema, add Card component, add sponsors

--- _global/schema/Event.ts
+++ _global/schema/Event.ts
@@ -1,15 +1,19 @@
 import { z } from 'astro:content';
-import { CardImageSchema } from '.';
+import { CardImageSchema, ICalDataSchema } from '.';
 
 export const EventSchema = ({ image }) => (
   z.object({
     title: z.string(),
-    series: z.string(),
-    description: z.string(),
+    ical: z.optional(ICalDataSchema()),
+    // discord_event: z.object({}),
     time_start: z.coerce.date(),
-    time_close: z.coerce.date(),
+    duration: z.string().default("PT48H"),
+    timezone: z.string().default("Etc/UTC"),
+    series: z.string(),
     credit: z.optional(z.array(z.string())),
+    sponsors: z.optional(z.array(z.string())),
     location: z.string(),
+    description: z.string(),
     card_image: z.optional(CardImageSchema(image)),
     links: z.array(z.object({
       name: z.string(),
