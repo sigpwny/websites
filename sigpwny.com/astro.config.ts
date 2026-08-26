c489ed457080925471e4b311b98412ba7558cf47
Minh Duong <github@whitehoodhacker.net>
2026-08-25 21:55:12 -0500

    feat(sigpwny): use Astro font API (#304)

--- sigpwny.com/astro.config.ts
+++ sigpwny.com/astro.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import { unified } from '@astrojs/markdown-remark';
 import react from "@astrojs/react";
@@ -35,6 +35,43 @@ export default defineConfig({
   ],
   output: 'static',
   cacheDir: './.cache',
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'Helvetica Neue',
+      cssVariable: '--font-helvetica-neue',
+      fallbacks: ['Helvetica', 'Arial', 'sans-serif'],
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/HelveticaNeue-Light.woff2'],
+            weight: 300,
+            style: 'normal',
+          },
+          {
+            src: ['./src/assets/fonts/HelveticaNeue-Regular.woff2'],
+            weight: 400,
+            style: 'normal',
+          },
+          {
+            src: ['./src/assets/fonts/HelveticaNeue-RegularItalic.woff2'],
+            weight: 400,
+            style: 'italic',
+          },
+          {
+            src: ['./src/assets/fonts/HelveticaNeue-Medium.woff2'],
+            weight: 500,
+            style: 'normal',
+          },
+          {
+            src: ['./src/assets/fonts/HelveticaNeue-Bold.woff2'],
+            weight: 700,
+            style: 'normal',
+          },
+        ],
+      },
+    },
+  ],
   redirects: redirects as any,
   trailingSlash: 'always',
   vite: {
